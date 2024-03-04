0d9a025e3ef72cc97f70775791e7ca3efa00f47f
julio correa <julio.correa@justa.com.vc>
2024-03-04 19:57:41 -0300

    add file node

---
+++ node/index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  queryDatabase("SELECT name FROM people", (error, nomes) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send("Erro ao buscar dados no banco de dados");
+    } else {
+      var listNames = "";
+      var h1 = `<h1>Full Cycle Rocks!</h1> \n`;
+      nomes.forEach((nome) => {
+        listNames = listNames + `<p> - ${nome} </p>\n`;
+      });
+      res.send(h1 + listNames);
+    }
+  });
+});
+
+app.post("/", (req, res) => {
+  const name = req.body.name;
+
+  if (!name) {
+    return res
+      .status(400)
+      .json({ error: 'Campo "name" não fornecido no corpo da requisição.' });
+  }
+
+  queryDatabase(
+    `INSERT INTO people(name) values('${name}')`,
+    (error) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send("Erro ao inserir dados no banco de dados");
+      }
+    }
+  );
+  res.status(201).json({ message: `O nome ${name} foi recebido com sucesso.` });
+});
+
+function queryDatabase(sql, callback) {
+  connection.query(sql, (error, results, fields) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      const nomes = results.map((row) => row.name);
+      callback(null, nomes);
+    }
+  });
+}
+
+app.listen(port, () => {
+  console.log("Rodando na porta " + port);
+});
